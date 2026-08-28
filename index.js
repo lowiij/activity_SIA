6348f6270519621f129901738a38e593704daa98
unknown <pc23@csitlab.local>
2026-08-28 11:21:00 +0800

    feat: add activity_SIA

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>My HTML File</title>
-</head>
-<body>
-    <h1>Hello World!</h1>
-    <p>This is my first HTML file.</p>
-</body>
-</html>
+// Package Imports
+const app = require('express')
+
+// Server Setup
+const server = app()
+
+server.listen(2007, '192.168.2.37' () => {
+    console.log("Server is running in 192.168.2.37:2007");
+})
+    
